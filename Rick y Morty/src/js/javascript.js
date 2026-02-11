c01530c5e0089d1b04f499adfecc13a028672e87
ImASimplePanda <thepandakwai@gmail.com>
2026-02-11 19:39:31 +0000

    Se programa el js para la api de rick y morty

--- Rick y Morty/src/js/javascript.js
+++ Rick y Morty/src/js/javascript.js
@@ -1,1 +1,26 @@
+const contenedor = document.querySelector('.contenedorTarjetas');
 
+fetch(`https://rickandmortyapi.com/api/character`)
+  .then(res => res.json())
+  .then(data => {
+    const personajes = data.results;
+
+    personajes.forEach(personaje => {
+      const tarjeta = document.createElement('article');
+      tarjeta.classList.add('tarjeta');
+
+      tarjeta.innerHTML = `
+        <img src="${personaje.image}">
+        <h2>${personaje.name}</h2>
+        <p>${personaje.status} - ${personaje.species}</p>
+        <p>${personaje.gender}</p>
+        <p>${personaje.origin.name}</p>
+      `;
+
+      contenedor.appendChild(tarjeta);
+
+    });
+  })
+  .catch(error => {
+    console.error('Error al cargar personajes:', error);
+  });
